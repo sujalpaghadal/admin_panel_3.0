e757cf96bd9e998a387a87c98680367e6ea783e9
KaushalS19 <kaushalsojitra19@gmail.com>
2024-06-19 11:48:27 +0530

    student list changes and api changes

--- src/api/student.js
+++ src/api/student.js
@@ -2,21 +2,25 @@ import useSWR from 'swr';
 import { useMemo } from 'react';
 
 import { fetcher } from '../utils/axios';
+import { useAuthContext } from '../auth/hooks/index.js';
 
 export function useGetStudents(page, limit) {
-  const URL = `https://admin-panel-dmawv.ondigitalocean.app/api/company//664ec61d671bf9a7f53664b5/student`;
+
+  const {user} = useAuthContext()
+
+  const URL = `https://admin-panel-dmawv.ondigitalocean.app/api/v2/${user.company_id}/student`;
 
   const { data, isLoading, error, isValidating } = useSWR(URL, fetcher);
 
   const memoizedValue = useMemo(
     () => ({
-      students: data?.data?.students || [],
+      students: data?.students || [],
       studentsLoading: isLoading,
       studentsError: error,
       studentsValidating: isValidating,
-      studentsEmpty: !isLoading && !data?.data?.students.length,
+      studentsEmpty: !isLoading && !data?.students.length,
     }),
-    [data?.data?.students, error, isLoading, isValidating],
+    [data?.students, error, isLoading, isValidating],
   );
 
   return memoizedValue;
